4eb1d815e553fb38a26a59334d74e64261554341
yangxiaofei <yangxiaofei@touch-spring.com>
2024-03-26 13:50:08 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import * as path from 'path';
 
 // https://vitejs.dev/config/
@@ -10,12 +7,6 @@ export default defineConfig({
   base: './',
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
   ],
   resolve: {
     // 路径别名配置
