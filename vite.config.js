0e66c55978e554e7030ff80719c944ed1cb96bfe
yangxiaofei <yangxiaofei@touch-spring.com>
2024-03-26 13:56:58 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,12 +7,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  resolve: {
-    // 路径别名配置
-    alias: {
-      '@': path.resolve('./src')
-    }
-  },
   build: {
     sourcemap: true,
     manifest: true,
