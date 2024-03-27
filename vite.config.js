c0fb6d30cd6844fa8e6a7f6dc84c0b19c2c65250
yangxiaofei <yangxiaofei@touch-spring.com>
2024-03-27 13:20:49 +0800

    test 修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/watermark-photo/',
+  base: '/watermark-photo/',
   plugins: [
     vue(),
   ],
